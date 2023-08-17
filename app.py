44d7f69947ff4685c00b69116ece874c34707071
Simon Woodward <Simonjwoodward5@gmail.com>
2023-08-17 17:27:47 +0100

    Added error handling 404 and 500 pages

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#website-env\Scripts\activate.bat
+#set FLASK_ENV=development
+#set FLASK_APP=app
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
@@ -7,3 +10,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+   return render_template('500.html'), 500
