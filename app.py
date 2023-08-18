2f67ceff51112ed64f9a15c1c0d4bc341c1df5be
Simon Woodward <Simonjwoodward5@gmail.com>
2023-08-18 23:12:50 +0100

    Added the contact us page

--- app.py
+++ app.py
@@ -3,21 +3,36 @@
 # set FLASK_APP=app
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, EmailField
+from wtforms.validators import DataRequired, Email, Length
 from werkzeug.exceptions import abort
+import config
 
 app = Flask(__name__)
 
 app.debug = True
+app.config['SECRET_KEY'] = config.secret_key
+
+
+class ContactForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired(), Email("Please check this field")])
+    phone_number = StringField("Telephone Number", validators=[DataRequired()])
+    description_of_query = TextAreaField("What can we help you with?", validators=[DataRequired(),
+        Length(-1, 600,"Please use less than 600 characters")])
+    submit = SubmitField("Submit")
 
 
 def plaintext_formatter(text_file_name):
-    file = open('static/' + text_file_name + '.txt', 'r')
+    file = open('static/textFiles/' + text_file_name + '.txt', 'r')
     return file.read().replace('\n', '<br>')
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    txt = plaintext_formatter('homepage')
+    return render_template('index.html', txt=txt)
 
 
 @app.route('/Domestic')
@@ -36,6 +51,26 @@ def commercial():
 def systems():
     txt = plaintext_formatter('systems')
     return render_template('domestic_commercial_systems.html', txt=txt)
+
+
+@app.route('/ContactUs', methods=['GET', 'POST'])
+def contact_us():
+    name = None
+    email = None
+    phone_number = None
+    description_of_query = None
+    form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        phone_number = form.phone_number.data
+        description_of_query = form.description_of_query.data
+        form.name.data = ""
+        form.email.data = ""
+        form.phone_number.data = ""
+        form.description_of_query.data = ""
+    return render_template('contact_us.html', name=name, email=email, phone_number=phone_number,
+                           description_of_query=description_of_query, form=form)
 
 
 @app.errorhandler(404)
